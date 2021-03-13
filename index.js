4c890f42fd763b123b2304032bce997934c13541
ThanhSon1612 <sonnguyen2766@gmail.com>
2021-03-13 19:43:35 +0700

    add static file

--- index.js
+++ index.js
@@ -9,25 +9,16 @@ app.set('views','./views')
 //Setting static forder
 app.use('/public',express.static('./public'))
 
-
-app.get('/Trangchu',(req,res) => {
-    res.send('<a href= "/chuyenmuc"> hello world</a>')
-})
-// trang chuyen mục
-app.get('/chuyenmuc',(req,res) =>{
-    res.send('chào mừng đén với chuyên mục')
+// Routes
+app.get('/',(req,res)=> {
+    res.render('client/index')
 })
 
-// Trang tuong tac
-app.get('/contact',(req,res) => {
-    res.send('liên hệ')
-})
-
-app.get('/contact/:name/:age',(req,res)=>{
-    const postid = res.params.name;
-    const post = res.params.age;
-    res.send('nguyễn thanh sơn ')
-})
+// app.get('/contact/:name/:age',(req,res)=>{
+//     const postid = res.params.name;
+//     const post = res.params.age;
+//     res.send('nguyễn thanh sơn ')
+// })
 
 app.listen(3000,() => {
     console.log('run');
